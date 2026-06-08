8a08951ed314a5f40fa46a5b762714652ca3962e
Shashank Shetty <shashankshetty3219@gmail.com>
2026-06-08 20:15:29 +0530

    Fix Render allowed host

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5175,
     strictPort: true,
-    host: true
+    host: true,
+  },
+
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: [
+      'hostelgo-flgw.onrender.com'
+    ]
   }
 })
